feba5598dc4c91e75c6064fdce62c50ef203e40c
pukbo <pakyalberico@gmail.com>
2026-08-21 15:10:59 +0200

    Initial commit: setup base Spring Boot in root corretta

---
+++ src/main/java/com/insurehub/agency_core/AgencyCoreApplication.java
@@ -0,0 +1,13 @@
+package com.insurehub.agency_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyCoreApplication.class, args);
+	}
+
+}
